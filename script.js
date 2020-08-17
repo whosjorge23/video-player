684b97e9d184d69c33a6ec9e7c38a00ec9ffa2a1
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2020-08-17 20:27:35 +0200

    Volume Mute/Unmute

--- script.js
+++ script.js
@@ -57,6 +57,9 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+
+let lastVolume = 1;
+
 // Volume Bar
 function changeVolume(e) {
   let volume = e.offsetX / volumeRange.offsetWidth;
@@ -78,6 +81,25 @@ function changeVolume(e) {
   } else if (volume === 0) {
     volumeIcon.classList.add("fas", "fa-volume-off");
   }
+  lastVolume = volume;
+}
+
+// Mute / Unmute
+function toggleMute() {
+  volumeIcon.className = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.className = "";
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Mute");
+  }
 }
 // Change Playback Speed -------------------- //
 
@@ -90,3 +112,4 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);

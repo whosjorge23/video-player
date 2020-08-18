e5ee5bac744558c8b1823d5e01871d7741b9ea05
Giorgio Giannotta <47144634+whosjorge23@users.noreply.github.com>
2020-08-18 13:09:41 +0200

    Add Playback Speed, Fullscreen, Download

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector('.player')
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -7,7 +8,9 @@ const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
+const speed = document.querySelector(".player-speed");
 const fullscreenButton = document.querySelector(".fullscreen");
+const downloadButton = document.querySelector(".download");
 
 // Play & Pause ----------------------------------- //
 function togglePlay() {
@@ -102,8 +105,62 @@ function toggleMute() {
   }
 }
 // Change Playback Speed -------------------- //
-
+function changeSpeed() {
+  video.playbackRate = speed.value
+}
 // Fullscreen ------------------------------- //
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+  video.classList.add('video-fullscreen')
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+  video.classList.remove('video-fullscreen')
+}
+
+let fullscreen = false
+
+// Toggle Fullscreen
+function toggleFullscreen() {
+  if(!fullscreen) {
+    openFullscreen(player)
+  }else{
+    closeFullscreen()
+  }
+  fullscreen = !fullscreen
+}
+
+// Download ------------------------------- //
+function downloadFile() {
+  let videoUrl = video.src
+  window.location.href = videoUrl
+  //  const videoA = document.createElement('a')
+  //  videoA.setAttribute('href', videoUrl)
+  //  videoA.setAttribute('download',videoUrl)
+  //  videoA.click
+}
 
 // Event Listeners
 playButton.addEventListener("click", togglePlay);
@@ -113,3 +170,6 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed)
+downloadButton.addEventListener("click",downloadFile)
+fullscreenButton.addEventListener("click",toggleFullscreen)
